f510bfa05c60dbab9b3cc11f78b88ac37e8f189a
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-11-01 10:33:42 +0545

    update: build system change to premake (basic)
    prev. from make to premake build system used now, configuration for the library (glfw) is done for mac only to search in default path (brew installed)

---
+++ premake5.lua
@@ -0,0 +1,20 @@
+workspace "imgui-space"
+    configurations { "Release" }
+
+project "imgui-base"
+    kind "consoleApp"
+    language "C++"
+
+    files { "**.cpp", "**.hpp" }
+
+    includedirs { "./", "./includes", "./backends" }
+
+    -- this linked library are explictly for the macos.. 
+    -- ensure opengl compatible and glfw is installed in path with brew...
+    configurations { "macosx" }
+        libdirs { "/opt/homebrew/lib", "/opt/local/lib", "/usr/local/lib" }
+
+        links { "glfw", "opengl.framework" }
+
+
+
