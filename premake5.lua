7c8d1220f3918d5bdd0974e5fa2da32d2319c9bc
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-11-01 12:41:28 +0545

    update: configed as static lib only
    currently without workspace the project is configed as the static library to be used, this will be the testing phase

--- premake5.lua
+++ premake5.lua
@@ -1,20 +1,8 @@
-workspace "imgui-space"
-    configurations { "Release" }
-
-project "imgui-base"
+project "imgui-sl"
     kind "StaticLib"
     language "C++"
-
-    files { "**.cpp", "**.hpp" }
-
-    includedirs { "./", "./includes", "./backends" }
-
-    -- this linked library are explictly for the macos.. 
-    -- ensure opengl compatible and glfw is installed in path with brew...
-    configurations { "macosx" }
-        libdirs { "/opt/homebrew/lib", "/opt/local/lib", "/usr/local/lib" }
-
-        links { "glfw", "opengl.framework" }
+    files { "./backends/*.cpp", "./*.cpp" }
+    targetdir "bin/%{cfg.buildcfg}"
 
 
 
