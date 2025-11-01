83e1577dfb01a4083c6b006488cb1f6ac8ee49fb
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-11-01 13:28:38 +0545

    fix: include dir for compilation

--- premake5.lua
+++ premake5.lua
@@ -2,6 +2,7 @@ project "imgui-sl"
     kind "StaticLib"
     language "C++"
     files { "./backends/*.cpp", "./*.cpp" }
+    includedirs { "./", "./backends", "./includes" }
     targetdir "bin/%{cfg.buildcfg}"
 
 
